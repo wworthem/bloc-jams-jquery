7a58e1a404e226e54513aafa666759c9ebbb7981
Warren <warrenworthem46@gmail.com>
2018-03-09 13:02:46 -0500

    Assignment 16 completed

--- scripts/album-info.js
+++ scripts/album-info.js
@@ -1,4 +1,6 @@
 {
   $('#album-title').text(album.title);
+  $('.artist').text(album.artist);
+  $('.album-details').text(album.releaseInfo);
   $('img#album-cover-art').attr('src', album.albumArtUrl);
 }
